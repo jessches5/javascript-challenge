abbddc8e4527db515781da76a78b474d72290da2
Jessica Chester <chesterjessica10@gmail.com>
2021-04-12 22:14:43 -0500

    appended td to row after defining proerties of ufoReport

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -3,8 +3,13 @@ var tableData = data;
 
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
+
 function buildTable(tableData) {
     tableData.forEach((ufoReport) => {
         var row = tbody.append("tr");
-        
+        Object.defineProperties(ufoReport).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+    });
+ });     
 }
