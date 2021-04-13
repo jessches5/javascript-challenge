535a8051e38a8c08c2fb1d60a0a7583f5e01457b
Jessica Chester <chesterjessica10@gmail.com>
2021-04-12 22:01:24 -0500

    appended table row

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -4,3 +4,7 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 function buildTable(tableData) {
+    tableData.forEach((ufoReport) => {
+        var row = tbody.append("tr");
+        
+}
